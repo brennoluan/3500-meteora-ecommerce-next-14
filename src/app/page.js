9beb31a27dea58c9abdec99ef40228ae35f76a84
brenno luan <brennoluan8530@gmail.com>
2026-06-16 20:53:50 -0300

    feat: implement API call to fetch produtos data in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
-import styles from "./page.module.css";
-import { Categorias } from "./components/Categorias";
-import { Produtos } from "./components/Produtos";
-import { getCategorias, getTodosProdutos } from "../lib/api";
+import styles from './page.module.css';
+import { Categorias } from './components/Categorias';
+import { Produtos } from './components/Produtos';
+import { getCategorias, getTodosProdutos } from '../lib/api';
 
+async function fetchProdutosApi() {
+  const res = await fetch('http://localhost:3000/api/produtos');
+
+  if (!res.ok) {
+    throw new Error('Nao foi possivel obter os dados');
+  }
+  const produtos = await res.json();
+
+  return produtos;
+}
 export default async function Home() {
-  const produtos = getTodosProdutos();
+  const { produtos } = await fetchProdutosApi();
   const categorias = getCategorias();
 
   return (
