675ed5c150b241f3eaaf8f41119d62231a20ae7f
brenno luan <brennoluan8530@gmail.com>
2026-06-16 21:16:49 -0300

    feat: refactor ProdutoPage to fetch product data using new API function

--- src/app/produto/[slug]/page.jsx
+++ src/app/produto/[slug]/page.jsx
@@ -1,9 +1,20 @@
-import styles from "./page.module.css";
-import Produto from "@/app/components/Produto";
-import { getProdutoPorSlug } from "../../../lib/api";
+import styles from './page.module.css';
+import Produto from '@/app/components/Produto';
+
+async function getProduto(slug) {
+  const res = await fetch(
+    'https://api.npoint.io/b5ea65a6b78807014009/produtos',
+  );
+
+  const produtos = await res.json();
+
+  const produto = produtos.find((produto) => produto.id.toString() === slug);
+
+  return produto;
+}
 
 export default async function ProdutoPage({ params }) {
-  const produto = getProdutoPorSlug(params.slug);
+  const produto = await getProduto(params.slug);
 
   return (
     <main className={styles.main}>
